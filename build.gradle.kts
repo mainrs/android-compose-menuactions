5138008ee71912a4fed0f6031d082f2add7eed87
mainrs <5113257+mainrs@users.noreply.github.com>
2022-06-18 22:31:26 +0200

    build: add maven artifact

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-plugins {
-    id("nl.littlerobots.version-catalog-update") apply(true)
-}
-
 tasks.create<Delete>("clean") {
     delete {
         rootProject.buildDir
--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,14 +1,29 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
+group = "com.github.mainrs"
+version = "1.0.0"
+
 android {
+    namespace = "net.zerotask.libraries.android.compose.menuactions"
     compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
+
+        aarMetadata {
+            minCompileSdk = libs.versions.compileSdk.get().toInt()
+        }
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
     }
 
     buildFeatures {
@@ -33,4 +48,20 @@ android {
 
 dependencies {
     implementation(libs.bundles.library)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                groupId = "com.github.mainrs"
+                artifactId = "android-compose-menuactions"
+                version = "1.0.0"
+
+                afterEvaluate {
+                    from(components["release"])
+                }
+            }
+        }
+    }
 }
